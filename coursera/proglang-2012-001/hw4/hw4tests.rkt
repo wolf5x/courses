74d7f27fec70609c5f22686a6d10fb4e8ec17281
wolf5x <wolf5x1016@gmail.com>
2013-02-23 03:01:46 +0800

    hw4+challenge finished, tested.

--- coursera/proglang-2012-001/hw4/hw4tests.rkt
+++ coursera/proglang-2012-001/hw4/hw4tests.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(provide (all-defined-out)) ;; so we can put tests in a second file
+
 (require "hw4.rkt") 
 
 ;; A simple library for displaying a 2x3 grid of pictures: used
@@ -66,3 +68,48 @@
 ; similar to previous but uses only two files and one position on the grid
 (define (visual-zero-only)
   (place-repeatedly (open-window) 0.5 (stream-add-zero dan-then-dog) 27))
+
+
+;; My Tests
+(equal? nums (list 0 1 2 3 4 5))
+(equal? (sequence 0 5 2) (list 0 2 4))
+(equal? (sequence 0 5 5) (list 0 5))
+(equal? (sequence 0 5 6) (list 0))
+(equal? (sequence 0 0 1) (list 0))
+(equal? (sequence 3 2 1) (list))
+
+files
+(equal? (string-append-map (list) ".jpg") (list))
+
+(equal? funny-test (list 1 2 3 4 -5 6 7 8 9 -10 11 12 13 14 -15 16))
+
+;(one-visual-test)
+
+;(visual-zero-only)
+
+(define vec1 (vector (cons 1 2) #t (cons (cons 9 8) 7) 6 "hi" (cons "ok" #f)))
+(equal? (vector-assoc 1 vec1) (cons 1 2))
+(equal? (vector-assoc (cons 9 8) vec1) (cons (cons 9 8) 7))
+(equal? (vector-assoc (cons 9 7) vec1) #f)
+
+(define vec2 (vector (cons 1 2) (cons #t 3) (cons (cons 9 8) 7) (cons 6 "hi") (cons "ok" #f)))
+(define lst2 (vector->list vec2))
+(define f10 (cached-assoc lst2 2))
+(f10 1)
+(f10 1)
+(f10 2)
+(f10 (cons 9 8))
+(f10 (cons 9 8))
+(f10 1)
+(f10 (cons 9 7))
+(f10 "ok")
+(f10 "ok")
+(f10 (cons 9 8))
+(f10 1)
+(f10 "ok")
+(f10 (cons 9 8))
+
+(define a 2)
+(while-less 7 do (begin (set! a (+ a 1)) (print "x") a))
+(while-less 7 do (begin (set! a (+ a 1)) (print "x") a))
+
